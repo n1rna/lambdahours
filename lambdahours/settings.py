463f91fedcf4c0616d5a2201726f464b1a299546
nima7r <n7r.distributed.ai@gmail.com>
2016-09-26 16:20:38 +0330

    shit got changed

--- lambdahours/settings.py
+++ lambdahours/settings.py
@@ -145,4 +145,4 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/' # It means home view
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
